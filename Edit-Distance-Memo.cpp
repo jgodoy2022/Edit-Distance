5c1530c801317828ce415e519e24b2f0ff7dca87
gzambrano2022 <gzambrano2022@udec.cl>
2025-05-31 03:54:17 -0400

    fix: agregué la documentación del Memoization

--- Edit-Distance-Memo.cpp
+++ Edit-Distance-Memo.cpp
@@ -4,32 +4,46 @@
 #include <string>
 using namespace std;
 
+// Función recursiva que calcula la cantidad de operaciones necesarias
+// para convertir s1 en s2.
 int editDistRec(string& s1, string& s2, int m, int n, vector<vector<int>> &memo){
 
+  // Si s1 está vacío, retorna n porque necesita n inserciones 
+  // para transformar s1 a s2. 
   if (m==0){
     return n;
   }
 
+  // Si s2 está vacío, retorna m porque necesita m eliminaciones
+  // para transformar s1 a s2. 
   if (n==0){
     return m;
   }
 
-  if (memo[m][n]!=-1){
+  // Verifica si ya se calculo la distancia para m,n. 
+  if (memo[m][n] != -1){
     return memo[m][n];
   }
 
+  // Si los últimos caracteres coinciden, avanzamos recursivamente 
+  // sin costo (no se suma 1), hasta alcanzar algún caso base. 
   if (s1[m-1]==s2[n-1]){
     return memo[m][n] = editDistRec(s1, s2, m-1, n-1, memo);
   }
 
+  // Si los últimos caracteres son diferentes, se toma el 
+  // mínimo de las dos operaciones y se suma 1. 
   return memo[m][n] = 1 + min({editDistRec(s1, s2, m, n-1, memo),
                               editDistRec(s1, s2, m-1, n, memo)});
 }
 
 
+// Función que inicia el calculo recursivo
 int editDistance(string& s1, string& s2){
   int m = s1.length(), n = s2.length();
-  vector<vector<int>> memo(m+1, vector<int>(n+1, -1));
+
+  // Inicializa la tabla(matriz) con -1. 
+  vector<vector<int>> memo(m+1, vector<int>(n+1, -1)); 
   return editDistRec(s1, s2, m, n, memo); 
 }
 
