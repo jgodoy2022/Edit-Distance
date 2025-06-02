e188b29dcb5e3e88b7846a478d6c0ad077f9d970
gzambrano2022 <gzambrano2022@udec.cl>
2025-06-02 12:51:08 -0400

    doc: edit distance dp documentación

--- Edit-Distance-DP.cpp
+++ Edit-Distance-DP.cpp
@@ -9,17 +9,20 @@ int editDistance(string &s1, string &s2){
   int m = s1.length();
   int n = s2.length();
 
+  // Se crea una matriz para guardar los resultados de
+  // la cantidad de operaciones
   vector<vector<int>> dp(m+1, vector<int>(n+1));
 
+  // inicializa la primera fila y columna, con 0 a m y con 0 a n, 
+  // respectivamente. 
   for(int i=0; i<=m; i++){
     dp[i][0] = i;
   }
-  
   for(int j=0; j<=n; j++){
     dp[0][j] = j;
   }
 
-
+  // llena el resto de la matriz, calculando la cantidad.
   for(int i=1; i<=m; i++){
     for(int j=1; j<=n; j++){
       if(s1[i-1] == s2[j-1]){
